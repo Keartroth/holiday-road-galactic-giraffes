6850706e100e53ff2f46337539422456e1da85db
Keartroth <mecarrolljr@gmail.com>
2020-03-31 11:01:42 -0500

    added a dialog button; added listen event to call a function to render the dialog component; added a listen event to close the dialog box; added some basic styling to park preview.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { getParks } from "./parks/ParkProvider.js";
 import { parkPreview } from "./parks/ParkPreview.js";
 import { ParkSelect } from "./parks/ParkSelect.js";
+import "./parks/ParkDialog.js";
 
 getParks()
   .then(ParkSelect)
