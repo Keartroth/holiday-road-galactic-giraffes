80c6c2d8e0c0a8a0ed29ff27bfda6250baa26a1f
iamdrewkirkland <drew.kirkland@gmail.com>
2020-03-31 14:43:32 -0400

    Updated fetch call to grab list. Also fixed static variable.

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,24 +1,22 @@
 //Import settings
 import { Settings } from "../Settings.js"
 
-const forecast = []
+let forecast = []
 
 // Function to export the data for other modules
 export const useWeather = () => {
     return forecast.slice();
 }    
 
-
-
 // Get the weather data
 export const getWeather = (postalCode) => {
 
-   return fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=${postalCode},us&units=imperial&appid=${Settings.weatherKey}`)
+   return fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${postalCode},us&units=imperial&appid=${Settings.weatherKey}`)
         .then(response => response.json())
         .then(
             parsedWeather => {
-                forecast = parsedWeather
-                console.table(forecast)
+                forecast = parsedWeather.list
+                console.log(forecast)
        }
    )
 }
