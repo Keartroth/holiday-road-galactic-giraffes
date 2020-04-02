f6ae89b073ed3cf281396b64985bbe40a25a300a
robdan89 <fuquardwork@gmail.com>
2020-04-02 16:11:09 -0500

    save button rendering and itinerary component complete.

---
+++ scripts/Itinerary/ItineraryList.js
@@ -0,0 +1,57 @@
+import { getParks, useParks } from "../parks/ParkProvider.js";
+import { getEateries, useEateries } from "../eateries/EateryProvider.js";
+import {
+  getAttractions,
+  useAttractions
+} from "../attractions/AttractionProvider.js";
+import { getItineraries, useItineraries } from "./ItineraryProvider.js";
+import { Itinerary } from "./Itinerary.js";
+
+const eventHub = document.querySelector(".container");
+const contentTarget = document.querySelector("#container__itinerary");
+
+export const ItineraryRender = () => {
+  let allTheEateries = [];
+  let allTheAttractions = [];
+  let allTheParks = [];
+
+  getEateries().then(() => {
+    allTheEateries = useEateries();
+  });
+  getAttractions().then(() => {
+    allTheAttractions = useAttractions();
+  });
+  getParks()
+    .then(getItineraries)
+    .then(() => {
+      allTheParks = useParks();
+
+      const allTheItineraries = useItineraries();
+
+      let itineraryObject = {};
+
+      for (const itinerary of allTheItineraries) {
+        const theFoundPark = allTheParks.find(currentParkObject => {
+          return currentParkObject.parkCode === itinerary.parkCode;
+        });
+        const theFoundEatery = allTheEateries.find(currentEateryObject => {
+          return currentEateryObject.id === itinerary.eateryId;
+        });
+        const theFoundAttraction = allTheAttractions.find(
+          currentAttractionObject => {
+            return currentAttractionObject.id === itinerary.attractionId;
+          }
+        );
+        itineraryObject.park = theFoundPark.name;
+        itineraryObject.eatery = theFoundEatery.businessName;
+        itineraryObject.attraction = theFoundAttraction.name;
+
+        contentTarget.innerHTML += Itinerary(itineraryObject);
+      }
+    });
+};
+
+eventHub.addEventListener("itineraryStateChanged", event => {
+  contentTarget.innerHTML = "";
+  ItineraryRender();
+});
---
+++ scripts/SaveItineraryButton.js
@@ -0,0 +1,51 @@
+const contentTarget = document.querySelector("#saveItineraryButtonContainer");
+const eventHub = document.querySelector(".container");
+
+// set three boolean variables for parks, eateries, and attractions
+
+let eateryChosenState = false;
+let attractionChosenState = false;
+
+const SaveItineraryButton = () => {
+  if (eateryChosenState && attractionChosenState) {
+    contentTarget.innerHTML = `
+        <button id="saveItineraryButton" type="button">Save Itinerary</button>
+        `;
+  }
+};
+
+//Build itinerary object
+//Properties will be populated by custom event listener details from parkChosenEvent, eateryChosenEvent, attractionChosenEvent
+
+let itineraryObject = {};
+
+// declare three listen events that listen for the custom events
+// parkChosenEvent, eateryChosenEvent, & attractionChosenEvent.
+
+eventHub.addEventListener("eateryChosenEvent", customEvent => {
+  const eateryId = parseInt(customEvent.detail.eatery);
+  itineraryObject.eateryId = eateryId;
+  eateryChosenState = true;
+  SaveItineraryButton();
+});
+
+eventHub.addEventListener("parkChosenEvent", customEvent => {
+  const parkAbbrev = customEvent.detail.park;
+  itineraryObject.parkCode = parkAbbrev;
+});
+
+eventHub.addEventListener("attractionChosenEvent", customEvent => {
+  const attractionId = parseInt(customEvent.detail.attraction);
+  itineraryObject.attractionId = attractionId;
+  attractionChosenState = true;
+  SaveItineraryButton();
+});
+
+contentTarget.addEventListener("click", clickEvent => {
+  if (clickEvent.target.id === "saveItineraryButton") {
+    const newItinerarySavedEvent = new CustomEvent("newItinerarySaved", {
+      detail: itineraryObject
+    });
+    eventHub.dispatchEvent(newItinerarySavedEvent);
+  }
+});
